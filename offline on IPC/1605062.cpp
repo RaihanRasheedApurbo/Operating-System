ffce98a41e4760e4ebc81f89b1edc47a95bc4550
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2020-09-29 09:53:07 -0400

    trying reading wpn to solve priority issue

--- offline on IPC/1605062.cpp
+++ offline on IPC/1605062.cpp
@@ -6,9 +6,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unsigned int numberOfBiker = 5;
+unsigned int numberOfBiker = 10;
 unsigned int numberOfServiceMen = 5;
 unsigned int numberOfCashier = 1;
+int wpn = 0; // wpn -> waiting people number
+pthread_mutex_t *wpnMutex= new pthread_mutex_t;
+pthread_mutex_t *enter = new pthread_mutex_t;
 pthread_mutex_t *serviceMen = new pthread_mutex_t[numberOfServiceMen];
 sem_t *cashier = new sem_t;
 void *bikerFunction(void *arg);
@@ -26,6 +29,7 @@ int main()
     for(int i=0;i<numberOfServiceMen;i++)
     {
         res = pthread_mutex_init(serviceMen+i,NULL);
+        sleep(10);
         if(res!=0)
         {
             printf("Mutex for serviceMen, creation failed!");
@@ -63,7 +67,14 @@ void *bikerFunction(void *arg)
 {
     int currentBikerNumber = *(int *)arg;
     delete (int *)arg;
+    //pthread_mutex_lock(enter);
+    
     pthread_mutex_lock(serviceMen+0);
+    if(wpn>0)
+    {
+        pthread_yield();
+        pthread_mutex_lock(serviceMen+0);
+    }
     for(int i=0;i<numberOfServiceMen;i++)
     {
         
@@ -77,14 +88,72 @@ void *bikerFunction(void *arg)
         {
             pthread_mutex_lock(serviceMen+i+1);
         }
+        // if(i==0)
+        // {
+        //     pthread_mutex_unlock(enter);
+        // }
         pthread_mutex_unlock(serviceMen+i);
     }
+    printf("%dth biker's servicing is complted waiting for payment\n",currentBikerNumber);
     sem_wait(cashier);
+    printf("%dth biker's is doing payment\n",currentBikerNumber);
     float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); // generates 0.0 to 1.0 inclusive
-    r = r*4; // generate 0.0 to 4.0 inclusive
-    sleep(r);
-    printf("payment completed of bikar %d\n",currentBikerNumber);
+    r = r*1; // generate 0.0 to 1.0 inclusive
+    //sleep(r);
     sem_post(cashier);
+    printf("payment completed of bikar %d\n",currentBikerNumber);
+    
+    pthread_mutex_lock(wpnMutex);
+    wpn++;
+    printf("%d\n",wpn);
+    if(wpn==1)
+    {
+        //pthread_mutex_lock(enter);
+        printf("hi\n");
+        for(int i=0;i<numberOfServiceMen-1;i++)
+        {
+            pthread_mutex_lock(serviceMen+i);
+        }
+
+        pthread_mutex_unlock(wpnMutex);
+        for(int i=numberOfServiceMen-1;i>=0;i--)
+        {
+            float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); // generates 0.0 to 1.0 inclusive
+            r = r*2; // generate 0.0 to 2.0 inclusive
+            sleep(r);
+            pthread_mutex_unlock(serviceMen+i);
+        }
+    }
+    else
+    {
+        pthread_mutex_unlock(wpnMutex);
+        pthread_mutex_lock(serviceMen+numberOfServiceMen-1);
+        for(int i=numberOfServiceMen-1;i>=0;i--)
+        {
+            float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); // generates 0.0 to 1.0 inclusive
+            r = r*2; // generate 0.0 to 2.0 inclusive
+            sleep(r);
+            if(i-1>=0)
+            {
+                pthread_mutex_lock(serviceMen+i-1);
+            }
+            pthread_mutex_unlock(serviceMen+i);
+        }
+        
+    }
+    pthread_mutex_lock(wpnMutex);
+    wpn--;
+    if(wpn==0)
+    {
+        //pthread_mutex_unlock(enter);
+    }
+    printf("biker number %d departed\n",currentBikerNumber);
+    pthread_mutex_unlock(wpnMutex);
+    
+    
+
+
+
     pthread_exit(0);
     
     
---
+++ offline on IPC/test.sh
@@ -0,0 +1,4 @@
+your_roll=1605062
+g++ -D _REENTRANT $your_roll.cpp -o $your_roll -lpthread
+./$your_roll
+rm $your_roll
